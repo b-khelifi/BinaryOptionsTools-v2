d60473734f3564548732fc6617919d53e898bd53
Rick-29 <ayfmportner@gmail.com>
2025-01-02 22:05:57 -0300

    minimal change

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,6 +1,7 @@
 pub use binary_option_tools_core::pocketoption::pocket_client::PocketOption;
 pub use binary_option_tools_core::pocketoption::ws::stream::StreamAsset;
-pub use binary_option_tools_core::pocketoption::types::order::Action;
+pub use binary_option_tools_core::pocketoption::types::order::{Action, Deal};
+pub use binary_option_tools_core::pocketoption::types::update::DataCandle;
 
 pub use binary_option_tools_core::pocketoption;
 pub use binary_option_tools_core::error::BinaryOptionsToolsError;
